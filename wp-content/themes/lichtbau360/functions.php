2b6a892c06c1cd03e0f27e6457ac3e29552c0d76
Fernando Cueto <fernandoecueto@gmail.com>
2024-05-08 02:06:27 +0200

    adds header and footer with styles for desktop layout

---
+++ wp-content/themes/lichtbau360/functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+function lichtbau360_files() {
+  wp_enqueue_style('custom-google-font', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+  wp_enqueue_style('lichtbau360_const_values', get_theme_file_uri('/css/constvalues.css'));
+  wp_enqueue_style('lichtbau360_main_styles', get_theme_file_uri('/css/index.css'));
+  wp_enqueue_style('lichtbau360_header_styles', get_theme_file_uri('/css/header.css'));
+  wp_enqueue_style('lichtbau360_footer_styles', get_theme_file_uri('/css/footer.css'));
+}
+
+add_action('wp_enqueue_scripts', 'lichtbau360_files');
+?>
--- wp-content/themes/lichtbau360/page.php
+++ wp-content/themes/lichtbau360/page.php
@@ -1,12 +1,9 @@
-<?php get_header() ?>
-
 <?php 
   while (have_posts()) {
     the_post(); ?>
+    <h1>Page, not a post</h1>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php the_content();?>
     <hr>
   <?php }
 ?>
-
-<?php get_footer()?>
