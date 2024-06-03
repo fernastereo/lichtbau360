9e55201aada7a1b61b132ad3f2133e84ba2ded76
Fernando Cueto <fernandoecueto@gmail.com>
2024-06-03 22:12:31 +0200

    feat: adjust slider styles for mobile

--- wp-content/themes/lichtbau360/front-page.php
+++ wp-content/themes/lichtbau360/front-page.php
@@ -1,50 +1,53 @@
 <?php get_header() ?>
 
 <main>
-<div class="slider-container">
-  <div class="slider-control left inactive"></div>
-  <div class="slider-control right"></div>
-  <ul class="slider-pagi"></ul>
   <div class="slider">
-    <div class="slide slide-0 active">
-      <div class="slide__bg" style="background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))">
-        <img class="slide__image" src="<?= get_theme_file_uri('/images/slider1.jpg') ?>" alt="">
-      </div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h2 class="slide__text-heading">Proyectando tus sueños</h2>
-          <p class="slide__text-desc"> Acompañamiento integral para todos tus proyectos.</p>
-          <p class="slide__text-content">Desarrollamos Proyectos, Asesoramos Jurídicamente y te Acompañamos Comercialmente, con nuestra asesoría experta, podrás contar un equipo altamente calificado y un acompañamiento integral de inicio a fin.</p>
-          <a class="slide__text-link">Contáctanos</a>
+    <ul class="slider-pagi">
+      <li class='slider-pagi__elem'></li>
+      <li class='slider-pagi__elem'></li>
+      <li class='slider-pagi__elem'></li>
+    </ul>
+    <div class="slides">
+      <div class="slide">
+        <div class="slide__bg" style="background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))">
+          <img class="slide__image" src="<?= get_theme_file_uri('/images/slider1.jpg') ?>" alt="">
+        </div>
+        <div class="slide__content">
+          <div class="slide__text">
+            <h2 class="slide__text-heading">Proyectando tus sueños</h2>
+            <p class="slide__text-desc">Acompañamiento integral para todos tus proyectos.</p>
+            <p class="slide__text-content">Desarrollamos Proyectos, Asesoramos Jurídicamente y te Acompañamos Comercialmente, con nuestra asesoría experta, podrás contar un equipo altamente calificado y un acompañamiento integral de inicio a fin.</p>
+            <a class="slide__text-link">Contáctanos</a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="slide slide-1">
-      <div class="slide__bg" style="background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))">
-        <img class="slide__image" src="<?= get_theme_file_uri('/images/slider2.jpg') ?>" alt="">
-      </div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h2 class="slide__text-heading">Disrupting Industries</h2>
-          <p class="slide__text-desc">WebCorpCo is all about making sure your marketing stack.</p>
-          <p class="slide__text-content">Desarrollamos Proyectos, Asesoramos Jurídicamente y te Acompañamos Comercialmente, con nuestra asesoría experta, podrás contar un equipo altamente calificado y un acompañamiento integral de inicio a fin.</p>
-          <a class="slide__text-link">Contáctanos</a>
+      <div class="slide">
+        <div class="slide__bg" style="background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))">
+          <img class="slide__image" src="<?= get_theme_file_uri('/images/slider2.jpg') ?>" alt="">
+        </div>
+        <div class="slide__content">
+          <div class="slide__text">
+            <h2 class="slide__text-heading">Proyectando tus sueños</h2>
+            <p class="slide__text-desc">Acompañamiento integral para todos tus proyectos.</p>
+            <p class="slide__text-content">Desarrollamos Proyectos, Asesoramos Jurídicamente y te Acompañamos Comercialmente, con nuestra asesoría experta, podrás contar un equipo altamente calificado y un acompañamiento integral de inicio a fin.</p>
+            <a class="slide__text-link">Contáctanos</a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="slide slide-2">
-      <div class="slide__bg" style="background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))">
-        <img class="slide__image" src="<?= get_theme_file_uri('/images/slider3.jpg') ?>" alt="">
-      </div>
-      <div class="slide__content">
-        <div class="slide__text">
-          <h2 class="slide__text-heading">Moving Business Forward</h2>
-          <p class="slide__text-desc">Sales is the direct result of marketing and sales data.</p>
-          <p class="slide__text-content">Desarrollamos Proyectos, Asesoramos Jurídicamente y te Acompañamos Comercialmente, con nuestra asesoría experta, podrás contar un equipo altamente calificado y un acompañamiento integral de inicio a fin.</p>
-          <a class="slide__text-link">Contáctanos</a>
+      <div class="slide">
+        <div class="slide__bg" style="background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))">
+          <img class="slide__image" src="<?= get_theme_file_uri('/images/slider3.jpg') ?>" alt="">
         </div>
+        <div class="slide__content">
+          <div class="slide__text">
+            <h2 class="slide__text-heading">Proyectando tus sueños</h2>
+            <p class="slide__text-desc">Acompañamiento integral para todos tus proyectos.</p>
+            <p class="slide__text-content">Desarrollamos Proyectos, Asesoramos Jurídicamente y te Acompañamos Comercialmente, con nuestra asesoría experta, podrás contar un equipo altamente calificado y un acompañamiento integral de inicio a fin.</p>
+            <a class="slide__text-link">Contáctanos</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </main>
 <?php get_footer()?> 
