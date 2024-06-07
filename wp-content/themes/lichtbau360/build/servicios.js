dbec124ec40b8193a4641686dcb4717e5f24fe48
Fernando Cueto <fernandoecueto@gmail.com>
2024-06-07 07:24:58 +0200

    feat: services section - juridica

---
+++ wp-content/themes/lichtbau360/build/servicios.js
@@ -0,0 +1,57 @@
+const servicesSlides = document.querySelectorAll('.servicios--slide');
+const juridica = document.querySelector('.servicios--slide.juridica');
+const proyectos = document.querySelector('.servicios--slide.proyectos');
+const inmobiliaria = document.querySelector('.servicios--slide.inmobiliaria');
+
+const transformSlides = (juridicaTransform, proyectosTransform, inmobiliariaTransform) => {
+  juridica.style.transform = juridicaTransform;
+  proyectos.style.transform = proyectosTransform;
+  inmobiliaria.style.transform = inmobiliariaTransform;
+};
+
+const showJuridica = () => {
+  transformSlides('translate(0%, 0%)', 'translate(100%, -100%)', 'translate(200%, -200%)');
+};
+
+const showProyectos = () => {
+  transformSlides('translate(-100%, 0%)', 'translate(0%, -100%)', 'translate(100%, -200%)');
+};
+
+const showInmobiliaria = () => {
+  transformSlides('translate(-200%, 0%)', 'translate(-100%, -100%)', 'translate(0%, -200%)');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  showProyectos();
+});
+
+/* ---------------------------------- */
+
+const clearActiveLinks = (links) => {
+  links.forEach((link) => {
+    link.classList.remove('active');
+  });
+};
+
+const addEventListenersToLinks = (selector) => {
+  const textLinks = document.querySelectorAll(selector);
+  textLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      clearActiveLinks(textLinks);
+      link.classList.add('active');
+
+      const tabs = document.querySelectorAll(`${selector.split(' ')[0]} .servicios__text`);
+      tabs.forEach((tab) => {
+        tab.classList.remove('active');
+      });
+
+      const tab = document.querySelector(`.servicios__text--${link.dataset.menu}`);
+      tab.classList.add('active');
+    });
+  });
+};
+
+addEventListenersToLinks('.juridica a');
+addEventListenersToLinks('.proyectos a');
+addEventListenersToLinks('.inmobiliaria a');
---
+++ wp-content/themes/lichtbau360/template-parts/sections/servicios.php
@@ -0,0 +1,158 @@
+<?php 
+$parametro1 = isset($args['parametro1']) ? 'pilars--' . $args['parametro1'] : '';
+$parametro2 = isset($args['parametro2']) ? 'pilars--' . $args['parametro2'] : '';
+
+?>
+<section class="servicios">
+  <h3 class="servicios-sub-header">Conoce nuestros</h2>
+  <h2 class="servicios-header">Servicios</h2>
+  <div class="servicios--slider">
+    <ul class="servicios-pagi">
+      <li id="juridica" class='servicios-pagi__elem' onclick="showJuridica()">
+        <?= servicios_icon('juridica') ?>
+      </li>
+      <li id="proyectos" class='servicios-pagi__elem' onclick="showProyectos()">
+        <?= servicios_icon('proyectos') ?>
+      </li>
+      <li id="inmobiliaria" class='servicios-pagi__elem' onclick="showInmobiliaria()">
+        <?= servicios_icon('inmobiliaria') ?>
+      </li>
+    </ul>
+    <div class="servicios--slides">
+      <div class="servicios--slide juridica">
+        <h2 class="servicios__title">Asesorias Juridicas</h2>
+        <div class="servicios__content">
+          <div class="servicios__content--menu">
+            <ul>
+              <li><a href="#" data-menu="proteccion" class="active">Proteccion Legal</a></li>
+              <li><a href="#" data-menu="tramites">Tramites</a></li>
+              <li><a href="#" data-menu="licitaciones">Licitaciones</a></li>
+            </ul>
+          </div>
+          <div class="servicios__text--container">
+            <div class="servicios__text servicios__text--proteccion active">
+              <h2 class="servicios__text--heading">Proteccion Legal</h2>
+              <div class="servicios__text--body">
+                <div class="text--body-1">
+                  <img class="image--body-1" src="<?php echo get_template_directory_uri() ?>/images/proteccion-legal.jpeg" alt="Proteccion Legal">
+                  <p>Diseñamos tus espacios de tu sueños, para que dese la estructura hasta las los detalles sean lo que deseas.</p>
+                  <a class="slide__text-link">Contáctanos</a>
+                </div>
+                <div class="text--body-2">
+                  <h3>Contratos</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <h3>Escrituras</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <h3>Promesas de Compra y Venta</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <div class="button-container">
+                    <a class="slide__text-link">Conoce Más</a>
+                  </div>
+                  <div class="image-container">
+                    <img class="image--body-2" src="<?php echo get_template_directory_uri() ?>/images/proteccion-legal-2.jpg" alt="Proteccion Legal">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="servicios__text servicios__text--tramites">
+              <h2 class="servicios__text--heading">Tramites</h2>
+              <div class="servicios__text--body">
+                <div class="text--body-1">
+                  <img class="image--body-1" src="<?php echo get_template_directory_uri() ?>/images/tramites.jpg" alt="Proteccion Legal">
+                  <p>Diseñamos tus espacios de tu sueños, para que dese la estructura hasta las los detalles sean lo que deseas.</p>
+                  <a class="slide__text-link">Contáctanos</a>
+                </div>
+                <div class="text--body-2">
+                  <h3>Contratos</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <h3>Escrituras</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <h3>Promesas de Compra y Venta</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <div class="button-container">
+                    <a class="slide__text-link">Conoce Más</a>
+                  </div>
+                  <div class="image-container">
+                    <img class="image--body-2" src="<?php echo get_template_directory_uri() ?>/images/tramites-2.jpg" alt="Proteccion Legal">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="servicios__text servicios__text--licitaciones">
+              <h2 class="servicios__text--heading">Licitaciones</h2>
+              <div class="servicios__text--body">
+                <div class="text--body-1">
+                  <img class="image--body-1" src="<?php echo get_template_directory_uri() ?>/images/licitaciones.jpg" alt="Proteccion Legal">
+                  <p>Diseñamos tus espacios de tu sueños, para que dese la estructura hasta las los detalles sean lo que deseas.</p>
+                  <a class="slide__text-link">Contáctanos</a>
+                </div>
+                <div class="text--body-2">
+                  <h3>Contratos</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <h3>Escrituras</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <h3>Promesas de Compra y Venta</h3>
+                  <p>Te acompañamos desde la arquitectura, el montaje y la construcción de obra blanca</p>
+                  <div class="button-container">
+                    <a class="slide__text-link">Conoce Más</a>
+                  </div>
+                  <div class="image-container">
+                    <img class="image--body-2" src="<?php echo get_template_directory_uri() ?>/images/licitaciones-2.jpg" alt="Proteccion Legal">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="servicios--slide proyectos">
+        <h2 class="servicios__title">Desarrollo de Proyectos</h2>
+        <div class="servicios__content">
+          <div class="servicios__content--menu">
+            <ul>
+              <li><a href="#" data-menu="obra-civil" class="active">Obra Civil</a></li>
+              <li><a href="#" data-menu="acabados">Acabados</a></li>
+              <li><a href="#" data-menu="diseno">Diseno de Interiores</a></li>
+              <li><a href="#" data-menu="remodelaciones">Remodelaciones</a></li>
+            </ul>
+          </div>
+          <div class="servicios__text--container">
+            <div class="servicios__text servicios__text--obra-civil active">
+              <h2 class="servicios__text--heading">Obra Civil</h2>
+              <div class="servicios__text--body"></div>
+            </div>
+            <div class="servicios__text servicios__text--acabados">
+              <h2 class="servicios__text--heading">Acabados</h2>
+            </div>
+            <div class="servicios__text servicios__text--diseno">
+              <h2 class="servicios__text--heading">Diseno de Interiores</h2>
+            </div>            
+            <div class="servicios__text servicios__text--remodelaciones">
+              <h2 class="servicios__text--heading">Remodelaciones</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="servicios--slide inmobiliaria">
+        <h2 class="servicios__title">Gestion Inmobiliaria</h2>
+        <div class="servicios__content">
+          <div class="servicios__content--menu">
+            <ul>
+              <li><a href="#" data-menu="venta" class="active">Venta</a></li>
+              <li><a href="#" data-menu="arriendos">Arriendos</a></li>
+            </ul>
+          </div>
+          <div class="servicios__text--container">
+            <div class="servicios__text servicios__text--venta active">
+              <h2 class="servicios__text--heading">Venta</h2>
+              <div class="servicios__text--body"></div>
+            </div>
+            <div class="servicios__text servicios__text--arriendos">
+              <h2 class="servicios__text--heading">Arriendos</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
