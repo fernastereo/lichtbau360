7e80f68d601dec6de84f452e60eda5519087a72c
Fernando Cueto <fernandoecueto@gmail.com>
2024-06-22 11:45:40 +0200

    feat: refactoring template parts

--- wp-content/themes/lichtbau360/page-nosotros.php
+++ wp-content/themes/lichtbau360/page-nosotros.php
@@ -1,6 +1,6 @@
 <?php 
   get_header();
-  get_template_part('template-parts/components/hero/hero', '') ?>
+  get_template_part('template-parts/components/hero/lb', 'hero') ?>
 
   <div class="container-nosotros">
     <p class="content">Somos mucho más que una empresa de desarrollo de proyectos. En LichtBau360, nos enorgullece ofrecer una amplia gama de servicios que  abarcan desde el asesoramiento jurídico hasta el acompañamiento  comercial.</p>
