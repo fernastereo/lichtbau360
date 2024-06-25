e5dd9a296ebef3753a4c4d87cbdea2bb15231969
Fernando Cueto <fernandoecueto@gmail.com>
2024-06-25 22:05:06 +0200

    add and support custom post type - propieddes

--- wp-content/mu-plugins/lichtbau_post_types.php
+++ wp-content/mu-plugins/lichtbau_post_types.php
@@ -13,10 +13,29 @@ function lichtbau360_post_types() {
       'all_items' => 'Todos los Proyectos',
       'singular_name' => 'Proyecto'
     ),
-    'menu_icon' => 'dashicons-bank',
+    'menu_icon' => 'dashicons-building',
     'supports' => array('title', 'editor', 'thumbnail'),
     'taxonomies' => array('category')
   ));
+
+  register_post_type('propiedad', array(
+    'supports' => array('title', 'excerpt', 'thumbnail'),
+    'rewrite' => array('slug' => 'propiedades'),
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Propiedades',
+      'add_new_item' => 'Nueva Propiedad',
+      'edit_item' => 'Editar Propiedad',
+      'all_items' => 'Todos las Propiedades',
+      'singular_name' => 'Propiedad'
+    ),
+    'menu_icon' => 'dashicons-admin-home',
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'taxonomies' => array('category')
+  ));
+
 }
 add_action('init', 'lichtbau360_post_types');
 ?>
