038f1a6b99a43bb380829916ac640b88fc5260c4
Fernando Cueto <fernandoecueto@gmail.com>
2024-07-02 16:38:35 +0200

    feat: change primary font and adjust styles on nosotros page

--- wp-content/themes/lichtbau360/functions.php
+++ wp-content/themes/lichtbau360/functions.php
@@ -2,7 +2,7 @@
 require get_template_directory() . '/helpers/helpers.php';
 
 function lichtbau360_files() {
-  wp_enqueue_style('custom-google-font', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+  wp_enqueue_style('custom-google-font', 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
   wp_enqueue_style('lichtbau360_const_values', get_theme_file_uri('/css/constvalues.css'));
   wp_enqueue_style('lichtbau360_main_styles', get_theme_file_uri('/css/index.css'));
   wp_enqueue_style('lichtbau360_header_styles', get_theme_file_uri('/css/header.css'));
